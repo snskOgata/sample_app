bd3e9a219dfd0d6c9739e659ffe8e92b8eed5a65
Shunsuke Ogata <snsk.ogata@gmail.com>
2019-11-30 02:24:03 +0000

    Disable wrong user to edit/update and Add friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -27,6 +27,11 @@ module SessionsHelper
     @current_user = nil
   end
   
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
   
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
@@ -45,6 +50,17 @@ module SessionsHelper
   # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
 end
